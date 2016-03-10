02520b5edcdb2c9fcf703f84a23442794c3ba1c3
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-03-10 07:45:34 -0700

    knex migration

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/album-demo'//enter in database when have
+    connection: 'postgres://localhost/marketing_users'
   },
 
   production: {
